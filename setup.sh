9221a9de4e187e8bddec5fa5bd8f2740435f6022
sarosx <dev.machine01@protonmail.com>
2022-08-26 22:17:15 +0000

    intital commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo pacman -S bspwm picom dunst nitrogen polybar sxhkd rofi zsh zsh-autosuggestoins zsh-syntax-highlighting pamixer
+cp -r .config .xinitrc $HOME
+chmod -R +x ~/.config/polybar/launch.sh
+chmod -R +x ~/.config/bspwm/scripts/* 
